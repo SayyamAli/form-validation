4b68742a42bb8c791c5e385acb843f787febf1a5
SayyamAli <SayyamAli@users.noreply.github.com>
2021-03-21 19:58:41 +0500

    update script.js

--- script.js
+++ script.js
@@ -13,33 +13,55 @@ function validateForm() {
         }
         ch = false;
     }
+    else{
+        $("#error-user").hide();
+        $("#username").css("border","1px solid #ced4da");
+        
+    }
     if (email == "") {
         $("#error-email").css("color", "red");
         $("#email").css("border", "2px solid red");
         $("#error-email").html("Please fill the Email field");
         ch = false;
     }
-    if (pass.length < 8) {
+    else{
+        $("#error-email").hide();
+        $("#email").css("border","1px solid #ced4da");
+    }
+    if (pass.length == 0) {
 
         $("#error-pass").css("color", "red");
         $("#password").css("border", "2px solid red");
-        if (pass == "") {
             $("#error-pass").html("Please fill the Password field");
-        }
         ch = false;
     }
-    if (c_pass == "") {
+    else if (pass.length < 8) {
+
+        $("#error-pass").css("color", "red");
+        $("#password").css("border", "2px solid red");
+        $("#error-pass").html("password must be atleast 8 characters long.");
+        ch = false;
+    }
+    else{
+        $("#error-pass").hide();
+        $("#password").css("border","1px solid #ced4da");
+    }
+    if (c_pass.length == 0) {
 
         $("#error-confirm-pass").css("color", "red");
         $("#confirm-password").css("border", "2px solid red");
         $("#error-confirm-pass").html("Please fill the Confirm Password field");
         ch = false;
     }
-    if (c_pass != pass) {
+    else if (c_pass != pass) {
         $("#error-confirm-pass").css("color", "red");
         $("#confirm-password").css("border", "2px solid red");
         $("#error-confirm-pass").html("Password does not match");
         ch = false;
+    }
+    else{
+        $("#error-confirm-pass").hide();
+        $("#confirm-password").css("border","1px solid #ced4da");
     }
     return ch;
 }
